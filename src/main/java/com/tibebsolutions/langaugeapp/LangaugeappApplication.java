e0a8d152d5ad3c2faeae229a6971e5a9742e475f
mulatusisay <mulatusisay1@gmail.com>
2021-05-04 13:40:20 +0300

    from pc to online

---
+++ src/main/java/com/tibebsolutions/langaugeapp/LangaugeappApplication.java
@@ -0,0 +1,13 @@
+package com.tibebsolutions.langaugeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangaugeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangaugeappApplication.class, args);
+    }
+
+}
